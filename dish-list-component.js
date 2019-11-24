3c9e0f5046aafb4253ac5ccff597779c88450fca
ArmandoClivo <armando@clivobot.com>
2019-11-24 05:58:38 -0600

    First commit

---
+++ dish-list-component.js
@@ -0,0 +1,21 @@
+import { LitElement } from '@polymer/lit-element';
+
+export class DishListComponent extends LitElement {
+    constructor() {
+        super();
+
+    }
+
+    static get properties() {
+        return {
+
+        };
+    }
+
+    render() {
+        return html `
+            
+        `;
+    }
+}
+customElements.define('dish-list-component', DishListComponent);
